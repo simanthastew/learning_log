7a323ed95b08f40ab7a5f675b55c767b75af4736
[01;31m[K"name"[m[K: "Samantha Stewart <[01;31m[K"email"[m[K: null>
2017-05-18 16:02:08 -0400

    create topic view and url

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -2,7 +2,17 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from .models import Topic
+#need line 5 to reference database models
 
 def index(request):
 	""" home page for learning log """
 	return render(request, 'learning_logs/index.html')
+
+def topics(request):
+	""" show all topics """
+	topics = Topics.objects.order_by('date_added')
+	context = {'topics': topics}
+	#context is a dictionary sent to the template where keys are names used in the template to access data
+	return render(request, 'learning_logs/topics.html', context)
+	#if you want context to be available, have to pass it to render function
