94ff4bee66a1c62416e56d9171646535c67a3528
[01;31m[K"name"[m[K: "Samantha Stewart <[01;31m[K"email"[m[K: null>
2017-05-18 09:13:51 -0400

    create topics model

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -3,4 +3,11 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Topic(models.Model):
+	""" a topic the user is learning about """
+	text = models.CharField(max_length=200)
+	date_added = models.DateTimeField(auto_now_add=True)
+
+	def __unicode__(self):
+		""" return string representation of the model """
+		return self.text
