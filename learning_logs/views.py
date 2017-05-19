36443585d624e387833f01e5446167271b9051d0
[01;31m[K"name"[m[K: "Samantha Stewart <[01;31m[K"email"[m[K: null>
2017-05-19 11:22:13 -0400

    create new topic template

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -2,8 +2,12 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
+
 from .models import Topic
 #need line 5 to reference database models
+from .forms import TopicForm
 
 def index(request):
 	""" home page for learning log """
@@ -25,3 +29,37 @@ def topic(request, topic_id):
 	#dash before date_added sorts entries in reverse order
 	context = {'topic': topic, 'entries': entries}
 	return render(request, 'learning_logs/topic.html', context)
+
+def new_topic(request):
+	""" add new topic """
+	if request.method != 'POST':
+		form = TopicForm()
+		#for get request, return blank form
+		#gave no arguments, so django creates blank form
+	else:
+		#post data submitted
+		form = TopicForm(request.POST)
+		if form.is_valid():
+			form.save()
+			return HttpResponseRedirect(reverse('learning_logs:topics'))
+			#if form is saved, redirect back to topics page
+
+	context = {'form': form}
+	#the context is a form regardless of get/post, so a blank form or the filled out form is the context, but on post request you get redirected
+	return render(request, 'learning_logs/new_topic.html', context)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
