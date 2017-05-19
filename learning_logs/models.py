3142cce60ae030d9202fdb93c804effe6b5c0da4
[01;31m[K"name"[m[K: "Samantha Stewart <[01;31m[K"email"[m[K: null>
2017-05-19 18:18:58 -0400

    add owner to topic and run migrations

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -2,11 +2,13 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
 
 class Topic(models.Model):
 	""" a topic the user is learning about """
 	text = models.CharField(max_length=200)
 	date_added = models.DateTimeField(auto_now_add=True)
+	owner = models.ForeignKey(User)
 
 	def __unicode__(self):
 		""" return string representation of the model """
