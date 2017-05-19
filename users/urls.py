30f1d0224c7d5b5ee8144268930341c34c41dd9a
[01;31m[K"name"[m[K: "Samantha Stewart <[01;31m[K"email"[m[K: null>
2017-05-19 15:31:13 -0400

    write logout url and view

--- users/urls.py
+++ users/urls.py
@@ -8,4 +8,6 @@ from . import views
 urlpatterns = [
 	#login page - different from other urls because it's a default django template
 	url(r'^login/$', login, {'template_name': 'users/login.html'}, name='login'),
+	#logout link
+	url(r'^logout/$', views.logout_view, name='logout'),
 ] 
--- users/views.py
+++ users/views.py
@@ -2,5 +2,13 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from django contribauth import logout
 
-# Create your views here.
+def logout_view(request):
+	#called logout_view so we can call django logout funciton later
+	""" log the user out """
+	logout(request)
+	#logout function is imported from django and does not need to be created
+	return HttpResponseRedirect(reverse('learning_logs:index'))
