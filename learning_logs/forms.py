b3ff22328ad39535183bfe4ffac092b98566bbed
[01;31m[K"name"[m[K: "Samantha Stewart <[01;31m[K"email"[m[K: null>
2017-05-19 11:42:47 -0400

    create entry form class

--- learning_logs/forms.py
+++ learning_logs/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
-from .models import Topic
+from .models import Topic, Entry
 
 class TopicForm(forms.ModelForm):
 	class Meta:
@@ -8,3 +8,11 @@ class TopicForm(forms.ModelForm):
 		fields = ['text']
 		labels = {'text': ''}
 		#tells django not to generate a label for the text field
+
+class EntryForm(forms.ModelForm):
+	class Meta:
+		model = Entry
+		fields = ['text']
+		labels = {'text': ''}
+		widgets = {'text': forms.Textarea(attrs={'cols': 80})}
+		#changes text field to text area
