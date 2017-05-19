76b65d697b2021fc8871de45f5bac35c7cabf957
[01;31m[K"name"[m[K: "Samantha Stewart <[01;31m[K"email"[m[K: null>
2017-05-19 11:44:03 -0400

    create new entry url

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -17,5 +17,7 @@ urlpatterns = [
 	#show individual topic^
 	url(r'^new_topic/$', views.new_topic, name='new_topic'),
 	#new topic page^
+	url(r'^new_entry/(?P<topic_id>\d+)/$', views.new_entry, name='new_entry'),
+	#new entry for specific topic ^
 
 ]
